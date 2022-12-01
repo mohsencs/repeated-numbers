dd0632e03790878c19d4de4e411c5b4f22d47dd7
Mohsen Soodkhah <mohsensoodkhah@gmail.com>
2022-12-01 22:10:49 +0330

    first commit

---
+++ src/main/java/ir/mofid/repeatedwords/RepeatedWordsApplication.java
@@ -0,0 +1,13 @@
+package ir.mofid.repeatedwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatedWordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepeatedWordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/mofid/repeatedwords/RepeatedWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mofid.repeatedwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatedWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
